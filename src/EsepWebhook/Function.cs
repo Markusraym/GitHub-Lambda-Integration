c27c8a8fe9a5ab22f3fb7e7707d04db911ae0df6
Markusraym <92881502+Markusraym@users.noreply.github.com>
2024-04-04 16:03:28 -0400

    Update Function.cs

--- src/EsepWebhook/Function.cs
+++ src/EsepWebhook/Function.cs
@@ -1,36 +1,41 @@
+using System.Net.Http;
 using System.Text;
 using Amazon.Lambda.Core;
-using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
-// Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
 
-namespace EsepWebhook;
-
-public class Function
+namespace EsepWebhook
 {
-    
-    /// <summary>
-    /// A simple function that takes a string and does a ToUpper
-    /// </summary>
-    /// <param name="input"></param>
-    /// <param name="context"></param>
-    /// <returns></returns>
-    public string FunctionHandler(object input, ILambdaContext context)
+    public class Function
     {
-        dynamic json = JsonConvert.DeserializeObject<dynamic>(input.ToString());
-        
-        string payload = $"{{'text':'Issue Created: {json.issue.html_url}'}}";
-        
-        var client = new HttpClient();
-        var webRequest = new HttpRequestMessage(HttpMethod.Post, Environment.GetEnvironmentVariable("SLACK_URL"))
+        /// <summary>
+        /// This function processes GitHub webhook events and sends a notification to Slack.
+        /// </summary>
+        /// <param name="input">The GitHub event payload.</param>
+        /// <param name="context">Lambda context information.</param>
+        /// <returns>The response from the Slack API.</returns>
+        public string FunctionHandler(JObject input, ILambdaContext context)
         {
-            Content = new StringContent(payload, Encoding.UTF8, "application/json")
-        };
+            // Extract issue details from the GitHub event
+            string issueTitle = input["issue"]?["title"]?.ToString();
+            string issueUrl = input["issue"]?["html_url"]?.ToString();
+            string issueBody = input["issue"]?["body"]?.ToString();
 
-        var response = client.Send(webRequest);
-        using var reader = new StreamReader(response.Content.ReadAsStream());
-            
-        return reader.ReadToEnd();
+            // Create a more descriptive message for Slack
+            string messageText = $"A new GitHub issue has been created:\n*Title:* {issueTitle}\n*Details:* {issueBody}\n*Link:* {issueUrl}";
+
+            // Construct the JSON payload to send to Slack
+            string payload = JsonConvert.SerializeObject(new { text = messageText });
+
+            // Send the message to Slack
+            using var client = new HttpClient();
+            var slackWebhookUrl = Environment.GetEnvironmentVariable("SLACK_URL");
+            var requestContent = new StringContent(payload, Encoding.UTF8, "application/json");
+            var response = client.PostAsync(slackWebhookUrl, requestContent).Result;
+
+            // Return the response from Slack
+            return response.Content.ReadAsStringAsync().Result;
+        }
     }
 }
